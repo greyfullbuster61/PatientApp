36d0b4f26bee8fb78cddc70a47e3c57ca9d564a7
greyf <greyf@DESKTOP-NFLTHJO>
2025-09-09 23:11:10 -0400

    Add project files.

---
+++ PatientApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Create a string variable called patientName
+        string patientName;
+
+        // 2. Ask who is being seen today
+        Console.WriteLine("Who is being seen today?");
+
+        // 3. Read what the user types
+        patientName = Console.ReadLine();
+
+        // 4. Write welcome message
+        Console.WriteLine("Welcome: " + patientName);
+    }
+}
